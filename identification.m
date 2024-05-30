6990b3e853c6731882cd3b4d7084eff26c496b0d
Cming-nl <coenmingelen30@gmail.com>
2024-05-30 12:13:21 +0200

    Veranderingen voor figuren

--- identification.m
+++ identification.m
@@ -7,7 +7,7 @@ f = load("sysid_data_cal/prbs_pos_2_long.mat");
 
 % Split training and validation data
 train_raw = f.data(6/h+1:80/h+1,:);
-t_train = train_raw(:,1);
+t_train = train_raw(:,1)-5;
 u_train = train_raw(:,2);
 theta_train = train_raw(:,3);
 phi_dot_train = train_raw(:,4);
@@ -17,12 +17,23 @@ iddata_data_train = iddata([theta_train, phi_dot_train], u_train, 0.05);
 f = load("sysid_data_cal/prbs_rand_0_25.mat");
 
 Tend = 120;
-val_raw = f.data(0/h+1:Tend/h,:);
-t_val = val_raw(:,1);
+val_raw = f.data(6/h+1:Tend/h,:);
+t_val = val_raw(:,1)-6;
 u_val = val_raw(:,2);
 theta_val = val_raw(:,3);
 phi_dot_val = val_raw(:,4);
 iddata_data_val = iddata([theta_val, phi_dot_val], u_val, 0.05);
+
+% Unpack the data and create a second iddata struct
+f = load("sysid_data_cal/chirp_1.mat");
+
+Tend = 40;
+val_raw = f.data(0/h+1:Tend/h,:);
+t_val2 = val_raw(:,1);
+u_val2 = val_raw(:,2);
+theta_val2 = val_raw(:,3);
+phi_dot_val2 = val_raw(:,4);
+iddata_data_val2 = iddata([theta_val2, phi_dot_val2], u_val2, 0.05);
 
 % Load estimated parameters
 f = load("params/simple_estimate_alphas_v2.mat");
@@ -50,24 +61,70 @@ sys_est = greyest(iddata_data_train, sys)
 x0_val = [theta_val(1), x_train(end, 2), phi_dot_val(1)];
 x0_val = [0,0,0];
 simdata_val = lsim(sys_est, u_val, t_val, x0_val);
+simdata_val2 = lsim(sys_est, u_val2,t_val2,x0_val);
 
 GOF_train = 100 * (1-goodnessOfFit(simdata_train, [theta_train, phi_dot_train], 'NRMSE'))
 GOF_val = 100 * (1-goodnessOfFit(simdata_val, [theta_val, phi_dot_val], 'NRMSE'))
+GOF_val2 = 100 * (1-goodnessOfFit(simdata_val2, [theta_val2, phi_dot_val2], 'NRMSE'))
+
+% figure()
+
+
+% subplot(2,1,1); hold on;
+% plot(t_train, theta_train);
+% plot(t_train, simdata_train(:,1));
+% xlim([0,75]);
+% xlabel('$t$ (s)', "Interpreter","latex","FontSize",15);
+% ylabel('$\theta$ (rad)', "Interpreter","latex","FontSize",15)
+% legend("Training data", "Simulation data","Location","northwest","Orientation",	"horizontal","FontSize",6)
+% subplot(2,1,2); hold on;
+% plot(t_train, phi_dot_train);
+% plot(t_train, simdata_train(:,2));
+% xlim([0,75]);
+% xlabel('$t$ (s)', "Interpreter","latex","FontSize",15);
+% ylabel('$\dot \phi$ (rad)', "Interpreter","latex","FontSize",15)
+% legend("Training data", "Simulation data","Location","northwest","Orientation",	"horizontal","FontSize",6)
+% sgtitle("Training data and model prediction")
+% exportgraphics(gcf,'0-Figures\Training data and model prediction.pdf') 
+% % saveas(gcf,'0-Figures\Training data and model prediction.pdf')
 
 figure(); 
-subplot(2,2,1); hold on;
-plot(t_train, theta_train);
-plot(t_train, simdata_train(:,1));
-subplot(2,2,3); hold on;
-plot(t_train, phi_dot_train);
-plot(t_train, simdata_train(:,2));
-
-subplot(2,2,2); hold on;
+subplot(2,1,1); hold on;
 plot(t_val, theta_val);
 plot(t_val, simdata_val(:,1));
-subplot(2,2,4); hold on;
+xlim([0,112])
+xlabel('$t$ (s)', "Interpreter","latex","FontSize",15);
+ylabel('$\theta$ (rad)', "Interpreter","latex","FontSize",15)
+legend("Validation data", "Simulation data","Location","northwest","Orientation",	"horizontal","FontSize",6)
+
+subplot(2,1,2); hold on;
 plot(t_val, phi_dot_val);
 plot(t_val, simdata_val(:,2));
+xlim([0,112])
+xlabel('$t$ (s)', "Interpreter","latex","FontSize",15);
+ylabel('$\dot \phi$ (rad)', "Interpreter","latex","FontSize",15)
+legend("Validation data", "Simulation data","Location","northwest","Orientation",	"horizontal","FontSize",6)
+
+sgtitle("PRBS validation data and model prediction")
+exportgraphics(gcf,'0-Figures\PRBS validation data and model prediction.pdf')
+
+figure(); 
+subplot(2,1,1); hold on;
+plot(t_val2, theta_val2);
+plot(t_val2, simdata_val2(:,1));
+xlabel('$t$ (s)', "Interpreter","latex","FontSize",15);
+ylabel('$\theta$ (rad)', "Interpreter","latex","FontSize",15)
+legend("Validation data", "Simulation data","Location","northwest","Orientation",	"horizontal","FontSize",6)
+
+subplot(2,1,2); hold on;
+plot(t_val2, phi_dot_val2);
+plot(t_val2, simdata_val2(:,2));
+xlabel('$t$ (s)', "Interpreter","latex","FontSize",15);
+ylabel('$\dot \phi$ (rad)', "Interpreter","latex","FontSize",15)
+legend("Validation data", "Simulation data","Location","northwest","Orientation",	"horizontal","FontSize",6)
+
+sgtitle("Chirp validation data and model prediction")
+exportgraphics(gcf,'0-Figures\Chirp validation data and model prediction.pdf')
 
 
 % Format the results and save them
